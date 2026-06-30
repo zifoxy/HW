36ce2ad138bf0ced045019788c73d8a0ada49431
zifoxy <thefunnylex@gmail.com>
2026-07-01 02:54:29 +0300

    -documents urls

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,22 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = 'Base Homework API',
+        default_verion='v0.1',
+        description='My Homework API',
+        terms_of_service='https://www.google.com/policies/terms',
+        contact=openapi.Contact(email='thefunnylex@gmail.com'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
 
 urlpatterns = [
     #base urls
@@ -26,5 +42,9 @@ urlpatterns = [
 
     #app urls
     path('users/', include('users.urls', namespace='users')),
-    
+
+    #documents urls
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
