52244758d6b368c07cbe386c4623e12566210e91
zifoxy <thefunnylex@gmail.com>
2026-07-01 02:21:29 +0300

    - add validation for password  - add serializers for user model's

---
+++ users/user_serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainSerializer
+from rest_framework_simplejwt.tokens import Token
+
+from users.models import User
+from users.validators import PasswordValidator
+
+class UserSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        fields = ('id', 'email', 'last_name', 'first_name', 'phone', 'is_active')
+    
+class UserCreateSerializer(serializers.ModelSerializer): 
+    password = serializers.CharField(write_only=True, required=True, min_length=8, max_length=16)
+
+    class Meta: 
+        model = User
+        fields = ('email', 'password',)
+        validators = [
+            PasswordValidator(field='password'),
+        ]
+
+    def create(self, validated_data): 
+        user = User.objects.create_user(**validated_data)
+        user.set_password(user.password)
+        user.save()
+        return user
+
+class UserTokenObtainPairSerializer(TokenObtainSerializer): 
+    @classmethod
+    def get_token(cls, user): 
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
---
+++ users/validators.py
@@ -0,0 +1,14 @@
+import re
+from django.core.exceptions import ValidationError
+
+class PasswordValidator:
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value): 
+        reg_pattern = re.compile(r'[A_Za-Z0-9]+$')
+        tmp_value = dict(value).get(self.field)
+        if not bool(reg_pattern.match(tmp_value)):
+            raise ValidationError("Ошибка: пароль должен содержать только латинские буквы и цифры")
+            
