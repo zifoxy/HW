a6da853686e07689a915801f84b5762c586b5444
zifoxy <thefunnylex@gmail.com>
2026-07-01 02:44:51 +0300

    - update base urls  - add model user to admin panel  - add users.urls

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'email', 'last_name', 'first_name')
+    list_filter = ('id', 'last_name', 'first_name')
+    ordering = ('id',)
+    
---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from users.apps import UsersConfig
+from users.views import UserListApiView, UserCreateAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView, UserTokenObtainPairView
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    #users.urlpatterns
+    path('', UserListApiView.as_view(), name='user_list'),
+    path('create/', UserCreateAPIView.as_view(), name='user_craete'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_retrieve'),
+    path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user_delete'),
+]
