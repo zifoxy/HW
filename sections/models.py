a5e0f2476a6bd9b0e0f53b20e9c353f7bad2b166
zifoxy <thefunnylex@gmail.com>
2026-07-01 11:51:56 +0300

    - models sections - start content_serializers

--- sections/models.py
+++ sections/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from users.models import NULLABLE
+
+class Section(models.Model): 
+    title = models.CharField(max_length=100, verbose_name=_('Title'), unique=True)
+    description = models.TextField(verbose_name=_('Description'), **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta: 
+        verbose_name = _('Section')
+        verbose_name_plural = _('Sections')
+        ordedring = ['id']
+
+class Content(models.Model): 
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name=_('Section'))
+    title = models.CharField(max_length=150, verbose_name=_('Title'), unique=True)
+    content = models.TextField(verbose_name=_('Content'))
+
+    def __str__(self):
+        return self.title
+
+    class Meta: 
+        verbose_name = _('Content')
+        verbose_name_plural = _('Contents')
+        ordering = ['id']
