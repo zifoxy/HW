2ace810e6534f251e05eddca9d33a00677ff7f0c
zifoxy <thefunnylex@gmail.com>
2026-07-01 12:01:04 +0300

    - add page's paginators

---
+++ sections/paginators.py
@@ -0,0 +1,9 @@
+from rest_framework.pagination import PageNumberPagination
+
+class SectionPaginator(PageNumberPagination):
+    page_size = 3 
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+class ContentPaginator(PageNumberPagination):
+    page_size = 5
