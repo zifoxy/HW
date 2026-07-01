4d971e84a533a0effcf376a9997aaf3c4bccf3bd
zifoxy <thefunnylex@gmail.com>
2026-07-01 12:38:24 +0300

    -add admin panel for sections

--- sections/admin.py
+++ sections/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from sections.models import Section, Content
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'title',)
+    list_filter = ('title',)
+    ordering = ('id',)
+    search_fields = ('title',)
+
+@admin.register
+class ContentAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'section', 'title',)
+    list_filter = ('section',)
+    ordering = ('id', 'section',)
+    search_fields = ('title',)
+    
