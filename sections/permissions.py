fae7a9178ea21e7e509c07ccaee7e4bdb01d5de3
zifoxy <thefunnylex@gmail.com>
2026-07-01 11:59:50 +0300

    -add level permissions

---
+++ sections/permissions.py
@@ -0,0 +1,21 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.permissions import BasePermission
+
+from users.models import UserRoles
+
+class IsModerator(BasePermission):
+    message = _('You are not a moderator')
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.role == UserRoles.MODERATOR:
+            return True
+        return False
+
+class IsSuperuser(BasePermission):
+    message = _('You are not a superuser')
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.is_superuser:
+            return True
+        return False
