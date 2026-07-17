38a061ac62fbe67c06a7e4c50f0beb4b4c1b53de
zifoxy <thefunnylex@gmail.com>
2026-07-17 18:51:31 +0300

    Update models.py
    clear with flake8

--- sections/models.py
+++ sections/models.py
@@ -1,22 +1,23 @@
-from tarfile import NUL
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from users.models import NULLABLE
 
-class Section(models.Model): 
+
+class Section(models.Model):
     title = models.CharField(max_length=100, verbose_name=_('Title'), unique=True)
     description = models.TextField(verbose_name=_('Description'), **NULLABLE)
 
     def __str__(self):
         return self.title
 
-    class Meta: 
+    class Meta:
         verbose_name = _('Section')
         verbose_name_plural = _('Sections')
         ordering = ['id']
 
-class Content(models.Model): 
+
+class Content(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name=_('Section'))
     title = models.CharField(max_length=150, verbose_name=_('Title'), unique=True)
     content = models.TextField(verbose_name=_('Content'))
@@ -24,22 +25,23 @@ class Content(models.Model):
     def __str__(self):
         return self.title
 
-    class Meta: 
+    class Meta:
         verbose_name = _('Content')
         verbose_name_plural = _('Contents')
         ordering = ['id']
 
-class Question(models.Model): 
+
+class Question(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name=_('Section'))
     description = models.TextField(verbose_name=_('Description'), **NULLABLE)
     question = models.TextField(verbose_name=_('Question'), **NULLABLE)
     answer = models.ForeignKey(Content, on_delete=models.CASCADE, verbose_name=_('Answer'), **NULLABLE)
     member_answer = models.TextField(verbose_name=_('Member Answer'), **NULLABLE)
 
-    def __str__(self): 
+    def __str__(self):
         return f'Вопрос по курсу {self.section}'
 
-    class Meta: 
+    class Meta:
         verbose_name = _('Question')
         verbose_name_plural = _('Questions')
         ordering = ['section',]
