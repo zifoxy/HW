370f351a01009ee39300360654cd89a2520cead0
zifoxy <thefunnylex@gmail.com>
2026-07-17 18:34:26 +0300

    Create test_03_question.py
    add test for questions

---
+++ sections/test_sections/test_03_question.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+import django
+django.setup()
+
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from sections.test_sections.utils import get_admin_user, get_member_user, get_test_content, get_test_question
+
+class QuestionTestCase(APITestCase):
+    def setUp(self):
+        self.user = get_admin_user()
+        response = self.client.post('/users/token/', {'email': self.user.email, 'password': 'qwerty'})
+        self.access_token = response.json().get('access')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+        self.question = get_test_question()
+
+    def test_16_question_list(self):
+        response = self.client.get('/question/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['results'][0]['question'], 'Test Question')
+
+    def test_17_question_detail(self):
+        response = self.client.get(f'/question/{self.question.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['question'], 'Test Question')
+
+    def test_18_question_is_correct(self):
+        correct_answer = {
+            'member_answer': 'Test Title Content',
+        }
+        wrong_answer = {
+            'member_answer': "wrong Title Content"
+        }
+        response = self.client.post(f'/question/{self.question.id}/', data=correct_answer)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('is_correct'), True)
+
+        response = self.client.post(f'/question/{self.question.id}/', data=wrong_answer)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('is_correct'), False)
+
+
+
+if __name__ == '__main__':
+    from django.conf import settings
+    from django.test.utils import get_runner
+
+    TestRunner = get_runner(settings)
+    failures = TestRunner(verbosity=2).run_tests(['sections.test_sections.test_03_question'])
+    sys.exit(bool(failures))
