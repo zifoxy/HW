9b6ddf6d50505868f7193803f719c1c99249d39a
zifoxy <thefunnylex@gmail.com>
2026-07-17 18:51:33 +0300

    Update paginators.py
    clear with flake8

--- sections/paginators.py
+++ sections/paginators.py
@@ -1,12 +1,15 @@
 from rest_framework.pagination import PageNumberPagination
 
+
 class SectionPaginator(PageNumberPagination):
-    page_size = 3 
+    page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 10
 
+
 class ContentPaginator(SectionPaginator):
     page_size = 5
+
 
 class QuestionPaginator(SectionPaginator):
     page_size = 5
