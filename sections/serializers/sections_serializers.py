d7e7218aa6c705eb46e5d12f039dd838dd2c964a
zifoxy <thefunnylex@gmail.com>
2026-07-17 18:51:42 +0300

    Update sections_serializers.py
    clear with flake8

--- sections/serializers/sections_serializers.py
+++ sections/serializers/sections_serializers.py
@@ -2,17 +2,19 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
 
 from sections.models import Section, Content
-from sections.serializers.content_serializers import ContentListSerializer, ContentSectionSerializer
+from sections.serializers.content_serializers import ContentSectionSerializer
+
 
 class SectionSerializer(ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Section
         fields = '__all__'
-    
+
+
 class SectionListSerializer(ModelSerializer):
     section_content_title = SerializerMethodField()
 
-    def get_section_content_title(self, section): 
+    def get_section_content_title(self, section):
         return ContentSectionSerializer(Content.objects.filter(section=section), many=True).data
 
     class Meta:
