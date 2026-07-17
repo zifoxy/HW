bf991eaf0e3b16256bc57969a0ae57574eaf67a6
zifoxy <thefunnylex@gmail.com>
2026-07-17 18:11:23 +0300

    Update user_serializers.py
    - fix bugs in user_serializers

--- users/user_serializers.py
+++ users/user_serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainSerializer
-from rest_framework_simplejwt.tokens import Token
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from users.models import User
 from users.validators import PasswordValidator
@@ -34,9 +33,9 @@ class UserUpdateSerialzier(serializers.ModelSerializer):
             PasswordValidator(field='password'),
         ]
 
-class UserTokenObtainPairSerializer(TokenObtainSerializer): 
+class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
-    def get_token(cls, user): 
+    def get_token(cls, user):
         token = super().get_token(user)
         token['email'] = user.email
         return token
