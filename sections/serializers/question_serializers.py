faac85a92a6713cadb30aa5cffbef3e02ba0f8b2
zifoxy <thefunnylex@gmail.com>
2026-07-17 18:51:38 +0300

    Update question_serializers.py
    clear with flake8

--- sections/serializers/question_serializers.py
+++ sections/serializers/question_serializers.py
@@ -1,9 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.relations import SlugRelatedField
-from rest_framework.fields import CharField, SerializerMethodField
+from rest_framework.fields import CharField
 
 from sections.models import Question, Section
-from sections.serializers.sections_serializers import SectionSerializer
+
 
 class QuestionSerializer(ModelSerializer):
     section = SlugRelatedField(slug_field='title', queryset=Section.objects.all())
@@ -12,11 +12,11 @@ class QuestionSerializer(ModelSerializer):
         model = Question
         fields = ('id', 'section', 'question')
 
+
 class QuestionSectionSerializer(ModelSerializer):
     section = SlugRelatedField(slug_field='title', queryset=Section.objects.all())
     members_answer = CharField()
 
-    class Meta: 
+    class Meta:
         model = Question
         fields = ('id', 'section', 'question', 'member_answer')
-
