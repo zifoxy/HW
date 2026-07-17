4c007496cbe3ea4dd2556c676d1608f9cc8c17b6
zifoxy <thefunnylex@gmail.com>
2026-07-17 18:51:37 +0300

    Update content_serializers.py
    clear with flake8

--- sections/serializers/content_serializers.py
+++ sections/serializers/content_serializers.py
@@ -3,20 +3,22 @@ from rest_framework.relations import SlugRelatedField
 
 from sections.models import Content, Section
 
+
 class ContentSerializer(ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Content
         fields = '__all__'
 
+
 class ContentSectionSerializer(ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Content
         fields = ('id', 'title')
+
 
 class ContentListSerializer(ModelSerializer):
     section = SlugRelatedField(slug_field='title', queryset=Section.objects.all())
 
-    class Meta: 
+    class Meta:
         model = Content
         fields = ('id', 'section', 'title')
-        
